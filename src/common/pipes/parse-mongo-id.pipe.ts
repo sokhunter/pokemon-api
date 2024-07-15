37df9579894df5bc8a841096504af17db0f6de57
Stefany Livaque <emylivaque@gmail.com>
2024-07-15 00:57:34 -0500

    eslint error resolve

--- src/common/pipes/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -1,9 +1,9 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
-  transform(value: string, metadata: ArgumentMetadata) {
+  transform(value: string) {
     if (!isValidObjectId(value)) {
       throw new BadRequestException(`${value} is not a valid mongoID`);
     }
